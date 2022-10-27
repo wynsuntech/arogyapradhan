e568976e8719c02cca84ce9577463c885ceb4e4a
TvishaTPL <103486046+TvishaTPL@users.noreply.github.com>
2022-10-27 15:28:12 +0530

    Delete admin

--- admin/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    port: 8083,
-    database: {
-        'database': 'techietalk_arogya_pradhan_db',
-        'username': 'techietalk_arogya_pradhan',
-        "password": 'Arogya@1212',
-        'host': "localhost"
-    }
-};
--- admin/static/assets/vendor/webimpress/safe-writer/src/Exception/ExceptionInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Webimpress\SafeWriter\Exception;
-
-interface ExceptionInterface
-{
-}
